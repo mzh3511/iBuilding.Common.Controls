f0b6f6c2f8e105b0933fcf9d7ac447bb418e6e20
Zhenhua Mao <zhenhua.mao@ranplan.net>
2018-05-28 10:44:45 +0800

    校准界面v0.0.0.1版本

---
+++ Test.WpfControl/Views/Bool2VisiblityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Test.WpfControl.Views
+{
+    public class Bool2VisiblityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? boolValue = (bool?)value;
+            if (boolValue.HasValue && boolValue.Value)
+                return Visibility.Visible;
+            return Visibility.Hidden;
+        }
+    }
+}
---
+++ Test.WpfControl/Views/CalibrationWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test.WpfControl.Views
+{
+    /// <summary>
+    /// CalibrationWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CalibrationWindow : Window
+    {
+        public CalibrationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
